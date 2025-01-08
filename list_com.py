17b7b38a7d57cb4bfba1e414042255622bba33af
Shivani11032006 <shivanisinghitc@gmail.com>
2025-01-08 04:57:38 +0000

    any name about project

---
+++ list_com.py
@@ -0,0 +1,8 @@
+res = [i for i in range(0,10)]
+print(res)
+# even numbers with list comprehension
+list = [12,2,36,4,523]
+res = [i for i in list if i < 4]
+print(res)
+res = [i for i in list if i % 2 == 0]
+print(res)
