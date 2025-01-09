e3fa6ef08ac47eaf7b3d6455e5ef0c67139852bf
Shivani11032006 <shivanisinghitc@gmail.com>
2025-01-09 04:48:08 +0000

    any name about project

--- add.py
+++ add.py
@@ -2,7 +2,7 @@ res = lambda x,y:x+y
 print(res(4,5))
 res = lambda x,y:x-y
 print(res(10,5))
-res = lambda x,y:x*y
-print(res(15,5))
+res = lambda x,y,z:x*y*z
+print(res(15,5,3))
 res = lambda x,y:x/y
 print(res(5,5))
